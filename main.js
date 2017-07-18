d5cc1c90cf6c632607b66f91da970eb83dbbbdfe
mklopman <michaelklopman@gmail.com>
2017-07-17 23:40:43 -0400

    basic outline done/1.5 pages done

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 $(function(){
 
 
-var body = $('#body');
+// $(".box").mouseover(function() {
+//   $(this).attr("src", "./images/britton.png");
+// }).mouseleave(function() {
+//     $(this).attr("src", "./images/machado.png");
+// });
 
-// const twitterLogo = $('<img>');
-// twitterLogo.attr('src', './images/twitter_logo.png');
+// window.sr = ScrollReveal();
+// sr.reveal('.spoon' { duration: 2000 });
 
-// const gitHubLogo = $('<img');
+// const projectOne = $('.spoon');
+// console.log(projectOne)
 
+// const logoGif = $('<img>')
+// logoGif.attr('src', './images/logos.gif')
+// logoGif.addClass('logos')
+// projectOne.append(logoGif);
 
-// body.append(twitterLogo)
+// const logosHeader = $('<h3>')
+// logosHeader.text("THE SPORTS LOGOS GAME")
+// logosHeader.addClass('logosHeader')
+// projectOne.append(logosHeader);
 
+window.sr = ScrollReveal({ reset: true });
 
-
-
-
-
-
-
-
-
-
-
+sr.reveal('.spoon', { duration: 2000 });
+sr.reveal('.fork', { duration: 4000 });
+sr.reveal('.knife', { duration: 6000 });
 
 
 
